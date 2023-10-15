e3542c12cbd8ccee3a1e1893a000f2f792e2c9e3
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-10-15 23:28:55 +0500

    Task 4

---
+++ Tyuiu.BotanogovDS.Sprint2.Task4.V9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BotanogovDS.Sprint2.Task4.V9.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint2.Task4.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine(" ИСХОДНЫЕ ДАННЫЕ:                                                      ");
+            Console.WriteLine("***********************************************************************");
+            
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine(" РЕЗУЛЬТАТ:                                                            ");
+            Console.WriteLine("***********************************************************************");
+            
+            Console.WriteLine("Значение функции " + res);
+            
+            Console.ReadKey();
+        }
+    }
+}
