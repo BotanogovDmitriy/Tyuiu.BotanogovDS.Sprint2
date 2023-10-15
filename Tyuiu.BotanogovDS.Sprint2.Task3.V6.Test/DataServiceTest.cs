02d88e8c1256880dc5084c43e9e57c45a415f0c3
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-10-15 23:13:37 +0500

    Task 3

---
+++ Tyuiu.BotanogovDS.Sprint2.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BotanogovDS.Sprint2.Task3.V6.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint2.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 2.615;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 1.413;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = 0.004;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -12;
+            double res = ds.Calculate(x);
+            double wait = -131.917;
+            Assert.AreEqual(wait, res);
+        }
+
+
+
+    }
+}
