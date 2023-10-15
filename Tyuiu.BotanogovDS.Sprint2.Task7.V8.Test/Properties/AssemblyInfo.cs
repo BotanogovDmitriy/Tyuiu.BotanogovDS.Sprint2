bc7e7b807e86066ebeddcfdba747a8edbf66d8ab
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-10-16 00:00:29 +0500

    Task 7

---
+++ Tyuiu.BotanogovDS.Sprint2.Task7.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BotanogovDS.Sprint2.Task7.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BotanogovDS.Sprint2.Task7.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f44b144-b32b-4cdf-aba0-0c5c5dbc4db6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
