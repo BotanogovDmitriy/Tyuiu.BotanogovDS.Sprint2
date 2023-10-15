dc3cca011f642c4be7b989348ac9b1031b89eaf0
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-10-15 23:53:04 +0500

    Task 6

---
+++ Tyuiu.BotanogovDS.Sprint2.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BotanogovDS.Sprint2.Task6.V8.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint2.Task6.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("4.10", ds.FindDateOfPreviousDay(10, 5));
+        }
+    }
+}
---
+++ Tyuiu.BotanogovDS.Sprint2.Task6.V8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BotanogovDS.Sprint2.Task6.V8.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint2.Task6.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine("Введите день: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите месяц: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if (((m < 1) || (m > 12)) || ((n < 1) || (n > 31)))
+            {
+                res = "Введены не верные значения";
+
+            }
+            else
+            {
+                res = "Предыдущая дата: " + ds.FindDateOfPreviousDay(m, n);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine(res);
+            
+            Console.ReadKey();
+        }
+    }
+}
